792aacbb983e39fe66e4674ddd64511ef9fb71e1
Michael De La Rosa <mdelarosa86@gmail.com>
2019-12-18 17:59:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
-function nowServing(line) {
+function nowServing(katzDeliLine) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
+  } else {
+    return `Currently serving ${line.shift()}.`
   }
-
-  return `Currently serving ${line.shift()}.`
 }
 
 function currentLine (line) {

af62f623d1073610dab139fefc89b5da8c32d135
Michael De La Rosa <mdelarosa86@gmail.com>
2019-12-18 18:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,16 +13,13 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine (line) {
-   if (line.length === 0) {
+  if (line.length === 0) {
     return "The line is currently empty."
-   }
-   
-   const numberName = []
-   
-   for (let i = 0; i < line.length; i++) {
-     numberName.push(`${i+1}. ${line[i]}`)
-   }
-   
-   return `The line is currently: ${numberName.join(', ')}`
-   
+  } else {
+    const numberName = []
+    for (let i = 0; i < line.length; i++) {
+      numberName.push(`${i+1}. ${line[i]}`)
+      return `The line is currently: ${numberName.join(', ')}`
+    }
+  }
 }
